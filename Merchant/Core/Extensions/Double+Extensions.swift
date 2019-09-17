57b6343ae7e13dbd960556e3958545392793feca
JCurativo <jen@bitcoin.com>
2019-09-17 15:40:03 +0800

    Update conversion

--- Merchant/Core/Extensions/Double+Extensions.swift
+++ Merchant/Core/Extensions/Double+Extensions.swift
@@ -10,7 +10,23 @@ import Foundation
 
 extension Double {
     func toSatoshis() -> Int64 {
-        return Int64(self*100000000)
+        guard let multiplier = Decimal(string: "100000000") else {
+            return Int64(self*100000000)
+        }
+        
+        let multiplicand = Decimal(floatLiteral: self)
+        let product = multiplicand * multiplier
+        return (product as NSDecimalNumber).int64Value
+    }
+    
+    func toBCH() -> Double {
+        guard let divisor = Decimal(string: "100000000") else {
+            return self / 100000000
+        }
+        
+        let dividend = Decimal(floatLiteral: self)
+        let quotient = dividend / divisor
+        return (quotient as NSDecimalNumber).doubleValue
     }
     
     func toString() -> String {
